b7a57127a010bf78dea5c6227401fae677f859d4
LMNatale1967 <LuisMiguelNatale@gmail.comm>
2022-08-16 20:02:10 +0200

    First Version With (click) Management To display Description

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -10,15 +10,20 @@ import { Component, OnInit } from '@angular/core';
 export class MainComponent implements OnInit {
 
   menuItems = [
-    {name: 'Pizza Margherita'},
-    {name: 'Pizza Double Cheese Margherita'},
-    {name: 'Pizzas Farm House'},
-    {name: 'Pizza Peppy PaneerSalads'},
-    {name: 'Pizza Mexican Green Wave'},
-    {name: 'Pizza Deluxe Veggie'},
-    {name: 'Pizza Veg Extravaganza'},
-    {name: 'Pizza CHEESE N CORN'} 
+    {name: 'Pizza Margherita', description: 'A hugely popular margherita, with a deliciously tangy single cheese topping.'},
+    {name: 'Pizza Double Cheese Margherita', description: 'The ever-popular Margherita - loaded with extra cheese'},
+    {name: 'Pizzas Farm House', description: 'A pizza that goes ballistic on veggies !'},
+    {name: 'Pizza Peppy Paneer', description: 'The best Mexican Peppy Paneer Pizza'},
+    {name: 'Pizza Mexican Green Wave', description: 'The best Mexican Green Wave Pizza'},
+    {name: 'Pizza Deluxe Veggie', description: 'The best Deluxe Veggie Pizza'},
+    {name: 'Pizza Veg Extravaganza', description: 'The best Veg Extravaganza Pizza'},
+    {name: 'Pizza CHEESE N CORN', description: 'The best CHEESE N CORN Pizza'} 
   ]
+
+  displayDescription(pDescription:string, pEvent:any) {
+    alert(pDescription);
+    console.log(pEvent);
+  }
 
   constructor() { 
   }
